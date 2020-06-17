901a2e22319d8380d18b339d4f8b51420872aaa7
leeli <leeli@leelis-iMac-2.local>
2020-06-17 22:59:14 +0800

    wxpay 0.15

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount Frontend::Engine   => '/', as: 'frontend'
   mount Product::Engine => '/', as: 'product'
   mount Admin::Engine => '/', as: 'admin'
+  mount Pay::Engine => '/', as: 'pay'
 
   # root to: "manager/conferences#index"
 end
