f50798b3eb098094478b4711baee5ce1d350ed68
leeli <oolzpishere@126.com>
2020-09-20 23:38:25 +0800

    update send_sms 3

--- components/admin/lib/admin/record_send_sms/qcloud_send_sms.rb
+++ components/admin/lib/admin/record_send_sms/qcloud_send_sms.rb
@@ -27,7 +27,7 @@ module Admin
       end
 
       def get_params_name
-        ::Admin::RecordSendSms::ParamsName.get_name(record, params_name)
+        Admin::RecordSendSms::ParamsName.get_name(record, params_name)
       end
 
       def get_params(params_name)
